e745db90f2d5b39673238e2ebbd03c10f172abe0
taharajati <taharjt.developer@gmail.com>
2025-05-22 20:07:38 +0330

    04

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const apiRoutes = require('./routes/api');
 require('./config/passport');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5003;
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/stock-analysis')
