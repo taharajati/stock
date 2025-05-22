a874e3c878be243fc26f63b476c03243cba3028a
taharajati <taharjt.developer@gmail.com>
2025-05-22 20:21:32 +0330

    04

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://taha.betaoption.ir:5003',
   withCredentials: true, // Important for cookies/session
   headers: {
     'Content-Type': 'application/json',
